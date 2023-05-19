81ecd17864104ae8f1509eeb9f0ba4ed90bcf31d
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-05-19 12:10:05 -0300

    feat: add ShortLinkForm and ShortLinkList to initial page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,25 @@
 import styled from 'styled-components'
+import { Ranking } from '../src/components/Ranking'
+import { ShortLinkForm } from '../src/components/ShortLinkForm'
+import { ShortLinkList } from '../src/components/ShortLinkList'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+const HomeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-bottom:100px;
+  form {
+    margin-top: calc(5% + 100px);
+    @media (max-width:450px){
+      margin-top: calc(5% + 50px);
+    }
+  }
 `
 
 export default function Home() {
-  return <Title>My page</Title>
+  return <HomeContainer>
+      {false && <Ranking />}
+      <ShortLinkForm />
+      <ShortLinkList />
+  </HomeContainer>
 }
